1be844dac7e52877ca2196000d7ed85a879d119b
Ruan David da Silva <112730142+Killjoybr@users.noreply.github.com>
2025-02-20 21:25:43 +0000

    moving to associated folder

--- Back/src/index.js
+++ Back/src/index.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
-app.get("/", (request, response) =>{
-    response.send("Test"); // testando uai
-    response.sendStatus(200); // 200 de ok
-})
+const appMessage = `Server running on port ${port}`;
 
 // Middleware -> pode ser usado na autenticação, validação, etc
 
